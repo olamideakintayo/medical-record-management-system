e1d32158511d01a3c6574213e8bf9bb6dfc45df8
Justice Igboneme <jayigboneme@gmail.com>
2025-07-31 00:49:46 -0700

    Revert "updated the entities fields and update the string method"

--- src/appointment.py
+++ src/appointment.py
@@ -1,35 +1,32 @@
+from doctor import Doctor
+from patient import Patient
 from datetime import datetime
 
 class Appointment:
 
-    def __init__(self, patient_id: int, doctor_id: str, appointment_date: datetime):
-        self.__patient_id = patient_id
-        self.__doctor_id = doctor_id
+    def __init__(self, patient: Patient, doctor: Doctor, appointment_date: datetime, description: str)->None:
+        self.__patient = patient
+        self.__doctor = doctor
         self.__appointment_date = appointment_date
-        self.__count: int = 0
-        self.__appointment_id = f"Appointment {self.__count}"
+        self.__description = description
 
-    @property
-    def get_appointment_id(self):
-        return self.__appointment_id
+        def patient(self):
+            return self.__patient
 
-    @get_appointment_id.setter
-    def get_appointment_id(self, appointment_number: int):
-        self.__count = appointment_number
-        self.__appointment_id = f"Appointment {self.__count}"
+        def doctor(self):
+            return self.__doctor
 
-    @property
-    def get_patient_id(self):
-        return self.__patient_id
+        @appointment_date.setter
+        def appointment_date(self, appointment_date):
+            self._appointment_date = appointment_date
 
-    @property
-    def get_doctor_id(self):
-        return self.__doctor_id
+        def get_appointment_date(self):
+            return self._appointment_date
 
-    @property
-    def get_appointment_date(self):
-        return self.__appointment_date
+        @description.setter
+        def description(self, description):
+            self.__description = description
 
-    def __str__(self):
-        message = (f"Patient ID: {self.__patient_id}\n Doctor ID: {self.__doctor_id}\n Appointment Date: {self.__appointment_date}\n")
-        return message
+        def get_description(self):
+            return self.__description
+
--- src/patient.py
+++ src/patient.py
@@ -1,25 +1,37 @@
 class Patient:
-    def __init__(self, name: str, contact_detail:str, date_of_birth: str, medical_history: str) -> None:
+
+    def __init__(self, name: str, date_of_birth: str, contact_details: str, medical_details: str)->None:
         self.__name = name
-        self.__contact_detail = contact_detail
         self.__date_of_birth = date_of_birth
-        self.__id = 0
+        self.__contact_details = contact_details
+        self.__medical_details = medical_details
+        self.__count = 0
 
-    def get_name(self) -> str:
-        return self.__name
+    def count(self):
+        self.__count += 1
 
-    def get_contact_detail(self) -> str:
-        return self.__contact_detail
+    def get_name(self):
+       return self.__name
 
-    def get_date_of_birth(self) -> str:
+    def get_date_of_birth(self):
         return self.__date_of_birth
 
-    def get_id(self) -> int:
-        return self.__id
+    def get_contact_details(self):
+        return self.__contact_details
 
-    def set_id(self, id: int) -> None:
-        self.__id = int(id)
+    def get_medical_details(self):
+        return self.__medical_details
 
-    def __str__(self) -> str:
-        message = (f" patient id: {self.__id}\n Patient Name: {self.__name}\n Contact Detail: {self.__contact_detail}\n Date of Birth: {self.__date_of_birth}\n Medical History: {self.__medical_history}\n")
+
+    def __str__(self):
+        message = (f"Patient Name: {self.__name}\n"
+                   f"Date of birth: {self.__date_of_birth}\n"
+                   f"Contact Details: {self.__contact_details}\n"
+                   f"Medical Details: {self.__medical_details}")
         return message
+
+
+
+
+helen = Patient("<NAME>", "2020-02-03", "080", "Head de pain me")
+print(helen.__str__())
